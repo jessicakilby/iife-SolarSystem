1188c0654014cd5a308ff1a106d677db4794cc81
jessica kilby <jessicakilby@gmail.com>
2016-08-20 11:27:46 -0500

    solar fun

---
+++ SolarSystem-Augmented.js
@@ -0,0 +1,15 @@
+//1. public property for holding the last mdified date
+//2. getter/setter for planets
+//3. getter/setter for activeSpacecraft
+//4. getter/setter for planetsLandedON
+
+
+var SolarSystem = (function(newSolarSystem) {
+	var planets = [];
+	var activeSpacecraft = [];
+	var planetsLandedOn = 0;
+	return {
+		
+	}
+
+})(SolarSystem || {});
---
+++ main.js
@@ -0,0 +1,4 @@
+console.log("SolarSytem", SolarSystem);
+console.log("Planets", SolarSystem.getPlanets);
+console.log("Get ActiveSpacecraft", SolarSystem.getActiveSpacecraft);
+console.log("Get PlanetsLandedOn", SolarSystem.getPlanetsLandedOn);
