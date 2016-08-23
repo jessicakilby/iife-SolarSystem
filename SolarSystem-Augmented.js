d76e930aa9cec0a912081aa58c2c5a2516d04e5d
jessica kilby <jessicakilby@gmail.com>
2016-08-22 20:41:03 -0500

    readme

--- SolarSystem-Augmented.js
+++ SolarSystem-Augmented.js
@@ -1,15 +1,36 @@
-//1. public property for holding the last mdified date
-//2. getter/setter for planets
-//3. getter/setter for activeSpacecraft
-//4. getter/setter for planetsLandedON
-
+//1. G/S for closest stars to the sun
+//2. G/S for age of solar system
+//3. G/S for known dwarf planets
 
 var SolarSystem = (function(newSolarSystem) {
-	var planets = [];
-	var activeSpacecraft = [];
-	var planetsLandedOn = 0;
-	return {
-		
-	}
+	var closestStars = ["Alpha Centauri"];
+	var ageOfSolarSystem = 0;
+	var dwarfPlanets = ["Pluto"];
+
+	newSolarSystem.getClosestStars = function() {
+		return closestStars;
+	};
+	newSolarSystem.setClosestStars = function(newClosestStars){
+		closestStars.push(newClosestStars);
+	};
+
+
+	newSolarSystem.getAgeOfSolarSystem = function() {
+		return ageOfSolarSystem;
+	};
+	newSolarSystem.setAgeOfSolarSystem = function(newAge){
+		ageOfSolarSystem = newAge;
+	};
+
+
+	newSolarSystem.getDwarfPlanets = function() {
+		return dwarfPlanets;
+	};
+	newSolarSystem.setDwarfPlanets = function(newDwarfPlanets){
+		dwarfPlanets.push(newClosestStars);
+	};
+
+	newSolarSystem.greeting = "bite me";
+	return newSolarSystem;
 
 })(SolarSystem || {});
